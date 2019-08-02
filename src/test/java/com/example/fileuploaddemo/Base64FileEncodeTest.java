93238c67094950ec89c048fb9e1b283d3821762a
greatbk <greatbk@daum.net>
2019-08-02 14:05:19 +0900

    .

--- src/test/java/com/example/fileuploaddemo/Base64FileEncodeTest.java
+++ src/test/java/com/example/fileuploaddemo/Base64FileEncodeTest.java
@@ -8,27 +8,30 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
 
 @Slf4j
 @RunWith(SpringRunner.class)
+@ActiveProfiles("local")
 @SpringBootTest
 public class Base64FileEncodeTest {
 
     private String testResourcePath;
+    private String serverBasePath;
 
     @Before
     public void setup() {
         testResourcePath = "./src/test/resources/images";
     }
 
-    private UploadFile createUploadFile(String base64String) {
+    private UploadFile createUploadFile(String base64String, String filename, String subPath) {
         UploadFile uploadFile = new UploadFile();
         uploadFile.setBase64String(base64String);
-        uploadFile.setSubPath("notice/10001");
-        uploadFile.setFilename("Alligator.jpg");
+        uploadFile.setSubPath(subPath);
+        uploadFile.setFilename(filename);
         uploadFile.setKeepOriginalFilename(true);
         return uploadFile;
     }
@@ -41,7 +44,7 @@ public class Base64FileEncodeTest {
         Assert.assertNotNull(base64String);
         log.debug(base64String);
 
-        UploadFile uploadFile = createUploadFile(base64String);
+        UploadFile uploadFile = createUploadFile(base64String, "Alligator.jpg", "notice/10001");
         String jsonString = UploadFile.toJson(uploadFile);
 
         Assert.assertNotNull(jsonString);
@@ -56,7 +59,7 @@ public class Base64FileEncodeTest {
         Assert.assertNotNull(base64String);
         log.debug(base64String);
 
-        UploadFile uploadFile = createUploadFile(base64String);
+        UploadFile uploadFile = createUploadFile(base64String, "big-file.zip", "notice/10002");
         String jsonString = UploadFile.toJson(uploadFile);
 
         Assert.assertNotNull(jsonString);
